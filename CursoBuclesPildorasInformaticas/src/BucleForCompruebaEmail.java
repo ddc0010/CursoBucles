8f9fe514e4e074ccfa0f4d250936693459ec4c3a
ddc0010 <ddc0010@alu.medac.es>
2024-06-03 11:06:38 +0200

    Todos los proyectos sobre bucles terminado

--- CursoBuclesPildorasInformaticas/src/BucleForCompruebaEmail.java
+++ CursoBuclesPildorasInformaticas/src/BucleForCompruebaEmail.java
@@ -4,17 +4,22 @@ public class BucleForCompruebaEmail {
 
 	public static void main(String[] args) {
 		
-		boolean arroba = false; 
+		int arroba = 0;
+		
+		boolean punto = false;
 		
 		String mail = JOptionPane.showInputDialog("Introduce mail"); 
 		
 		for (int i = 0; i < mail.length();i++) {
 			if (mail.charAt(i)=='@') {
-				arroba = true; 
+				arroba++; 
 			}
-		}
 		
-		if (arroba == true) {
+		if (mail.charAt(i)=='.') {
+			punto = true; 
+		}
+		}
+		if(arroba == 1 && punto == true) {
 			System.out.println("Es correcto");
 		}
 		else {
---
+++ CursoBuclesPildorasInformaticas/src/FactorialUsandoFor.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class FactorialUsandoFor {
+
+	public static void main(String[] args) {
+		
+		int resultado = 1; 
+		
+		int numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero"));
+		
+		for (int i = numero; i > 0; i--) {
+			resultado = resultado * i;
+		}
+		System.out.println("El factorial de " + numero + " es " + resultado);
+
+	}
+
+}
